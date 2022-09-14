a717278727d4f2fd98f631bc411a3ef5bd686355
abujayed007 <abujayed007@gmail.com>
2022-09-15 04:58:32 +0600

    completed history section responsive

--- history.js
+++ history.js
@@ -1,8 +1,10 @@
 const histories = document.getElementById("histories");
+// console.log(histories)
 
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
+  // console.log(timeTaken)
 
   newRow.innerHTML = `
   <h3>${questionText}</h3>
@@ -26,12 +28,13 @@ function displayHistory() {
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
 
   previousTests.forEach((test) => {
+    // console.log(test)
     const newRow = document.createElement("div");
     newRow.classList.add("card");
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${parseInt(test.timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const typeController = (e) => {
     return;
   }
 
-  userText += newLetter;
+  userText = newLetter;
 
   const newLetterCorrect = validate(newLetter);
 
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken =parseInt(finishTime - startTime) / 1000;
 
   // show result modal
   resultModal.innerHTML = "";
@@ -80,7 +80,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML = `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -135,5 +135,5 @@ setInterval(() => {
   const timeSpent =(currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
